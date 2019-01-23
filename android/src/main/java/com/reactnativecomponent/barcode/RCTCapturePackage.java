51e6048561085a69a421a48e52bf36c254b0f7a8
xukj <xukj002@forsealife.com>
2019-01-23 11:12:58 +0800

    compatible rn >= 0.47.0

--- android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
+++ android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
@@ -29,26 +29,25 @@ public class RCTCapturePackage implements ReactPackage {
         captureManager = new RCTCaptureManager();
     }
 
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+            mModuleInstance = new RCTCaptureModule(reactApplicationContext,captureManager);
+    return Arrays.<NativeModule>asList(
+            mModuleInstance
+    );
+    }
+
+    // @Override deprecate: rn 0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
     @Override
-        public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
-             mModuleInstance = new RCTCaptureModule(reactApplicationContext,captureManager);
-        return Arrays.<NativeModule>asList(
-                mModuleInstance
-        );
-        }
-
-        @Override
-        public List<Class<? extends JavaScriptModule>> createJSModules() {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
-            //noinspection ArraysAsListWithZeroOrOneArgument
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        //noinspection ArraysAsListWithZeroOrOneArgument
 
 //            return Arrays.<ViewManager>asList(captureManager,linearGradientViewManager);
-            return Arrays.<ViewManager>asList(captureManager);
-        }
-
+        return Arrays.<ViewManager>asList(captureManager);
     }
+
+}
